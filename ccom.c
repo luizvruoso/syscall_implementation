16a2391b034c3aca7520a9a7a0e0ce9c25f8da2b
GabrielGMSantini <53902751+GabrielGMSantini@users.noreply.github.com>
2020-10-27 17:26:00 -0300

    foi de base
    Pequenas alterações no código da base e implementação de um programa simples de teste.

--- ccom.c
+++ ccom.c
@@ -6,9 +6,9 @@
 #include "com.h"
 
 asmlinkage ssize_t write_crypt(int fd, const void*buf, size_t nbytes){
-    return write(fd,buf,nbytes);
+    return sys_write(fd,buf,nbytes);
 }
 
 asmlinkage ssize_t read_crypt(int fd, void*buf, size_t nbytes){
-    return read(fd,buf,nbytes);
+    return sys_read(fd,buf,nbytes);
 }
---
+++ test.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+int main()
+{  
+     char text[100];
+    printf("Invoking 'listProcessInfo' system call");
+    int fd = open("./aqui.txt", O_WRONLY|O_CREAT, 0666);
+     printf("Digite um texto [max. 100 carac.]: ");
+	scanf("%[^\n]", text);
+
+    long int ret_status = syscall(334,fd,text,strlen(text)); 
+    close(fd);
+    fd = open("./aqui.txt", O_RDONLY|O_CREAT, 0666);
+    char rec[100];
+    long int ret_status2 = syscall(335,fd,rec,strlen(text));
+    printf("%s",rec);
+         
+    if(ret_status >= 0) 
+         printf("System call 'write_crypt' executed correctly. Use dmesg to check processInfo\n");
+    
+    else 
+         printf("System call 'write_crypt' did not execute as expected\n");
+
+    if(ret_status2 >= 0) 
+         printf("System call 'read_crypt' executed correctly. Use dmesg to check processInfo\n");
+    
+    else 
+         printf("System call 'read_crypt' did not execute as expected\n");
+          
+          
+     return 0;
+}
