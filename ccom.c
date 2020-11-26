6ca2f76d278e417af88c96d056e1f27fb242254d
Luiz Vinícius Ruoso <53343378+luizvruoso@users.noreply.github.com>
2020-11-26 10:40:54 -0300

    Update ccom.c

--- ccom.c
+++ ccom.c
@@ -1,3 +1,11 @@
+/*
+Gabriel Gonçalves Mattos Santini -  18189084
+Luiz Vinicius dos Santos Ruoso - 18233486
+Marcelo Germani Olmos -  18048298    
+Victor Felipe dos Santos -  18117820
+Victor Luiz Fraga Soldera - 18045674
+*/
+
 #include<linux/kernel.h>
 #include<linux/init.h>
 #include<linux/sched.h>
