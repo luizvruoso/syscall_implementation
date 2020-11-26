83cef7073c89321aeef95ea23e1361c9873b0614
Luiz Vinícius Ruoso <53343378+luizvruoso@users.noreply.github.com>
2020-11-26 10:46:59 -0300

    Update processInfo.h

--- processInfo.h
+++ processInfo.h
@@ -1,2 +1,10 @@
+/*
+Gabriel Gonçalves Mattos Santini -  18189084
+Luiz Vinicius dos Santos Ruoso - 18233486
+Marcelo Germani Olmos -  18048298    
+Victor Felipe dos Santos -  18117820
+Victor Luiz Fraga Soldera - 18045674
+*/
+
 asmlinkage long read_crypt(int fd,char *buf, size_t nbytes);
 asmlinkage long write_crypt(int fd,char *buf, size_t nbytes);
