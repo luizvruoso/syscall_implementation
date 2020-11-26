88b6c05758e637879cceec1a66ddbdc6b36bdfc6
Luiz Vinícius Ruoso <53343378+luizvruoso@users.noreply.github.com>
2020-11-26 10:41:33 -0300

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,12 @@
+/*
+Gabriel Gonçalves Mattos Santini -  18189084
+Luiz Vinicius dos Santos Ruoso - 18233486
+Marcelo Germani Olmos -  18048298    
+Victor Felipe dos Santos -  18117820
+Victor Luiz Fraga Soldera - 18045674
+*/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
